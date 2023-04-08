ea098f079b1f0f8471e1b3fd937d8be2d869d02d
CarlesRojas <carlesrojas@outlook.com>
2023-04-08 10:43:38 +0200

    use optimistic updates to remove from the list

--- src/hooks/api/mutation/useSetOrderAsDelivered.ts
+++ src/hooks/api/mutation/useSetOrderAsDelivered.ts
@@ -8,6 +8,21 @@ const useSetOrderAsDelivered = () => {
     isLoading: isLoadingSetOrderAsDelivered,
     isError: isErrorSetOrderAsDelivered,
   } = api.private.setOrderAsDelivered.useMutation({
+    onMutate: async ({ orderId }) => {
+      await apiContext.private.getOrdersToDeliver.cancel();
+
+      const previousOrders = apiContext.private.getOrdersToDeliver.getData();
+      if (!previousOrders) return { previousOrders };
+
+      const newOrders = previousOrders.filter((order) => order.id !== orderId);
+
+      apiContext.private.getOrdersToDeliver.setData(undefined, newOrders);
+
+      return { previousOrders };
+    },
+    onError: (err, data, context) => {
+      apiContext.private.getOrdersToDeliver.setData(undefined, context?.previousOrders);
+    },
     onSettled: () => {
       void apiContext.private.getOrdersToDeliver.invalidate();
     },
